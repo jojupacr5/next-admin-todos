41357021be8ee22814bda14b83b0a7a49c2ed1cf
jojupacr5 <joseph.paucar@gmail.com>
2024-08-24 23:04:16 -0500

    Se agregaron cookies desde el lado del servidor para mantener el valor

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from "@/components";
+import { cookies } from 'next/headers'
 
 export const metadata = {
  title: 'Cookies Page',
@@ -6,12 +7,17 @@ export const metadata = {
 };
 
 export default function CookiesPage() {
+
+  const cookieStore = cookies()
+  const cookieTab = Number(cookieStore.get('selectedTab')?.value ?? '1');
+
+
   return (
     <div className="grid grid-cols-1 gap-3">
 
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={cookieTab} />
       </div>
       
     </div>
